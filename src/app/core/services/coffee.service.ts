06cf09764be5b88f2330a7ede6ae7b6b01430653
Bondoc Daniel <daniel.bondoc.it@gmail.com>
2025-04-21 12:18:07 +0300

    add Coffee Services

---
+++ src/app/core/services/coffee.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, signal } from '@angular/core';
+
+export interface Coffee {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  imageUrl: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoffeeService {
+  private coffees = signal<Coffee[]>([
+    {
+      id: 1,
+      name: 'Espresso',
+      description: 'Strong and bold coffee.',
+      price: 2.5,
+      imageUrl: 'placeholder1.jpg' // replace with image later
+    },
+    {
+      id: 2,
+      name: 'Cappuccino',
+      description: 'Espresso with steamed milk foam.',
+      price: 3.0,
+      imageUrl: 'placeholder2.jpg'
+    }
+  ]);
+
+  getCoffees() {
+    return this.coffees;
+  }
+
+  searchCoffee(term: string): Coffee[] {
+    return this.coffees().filter(c =>
+      c.name.toLowerCase().includes(term.toLowerCase())
+    );
+  }
+}
